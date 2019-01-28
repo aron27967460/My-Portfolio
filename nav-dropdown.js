89b026dc52a991cc23d92f190a2b41585cc06eb3
Unknown <aron27967460@hotmail.com>
2019-01-28 04:08:35 -0800

    overlay interaction and minor pbook.html

--- nav-dropdown.js
+++ nav-dropdown.js
@@ -1,13 +1,11 @@
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-    document.getElementById("myNav").style.top = "0";
+    document.getElementById("myNav").style.left = "0%";
     document.getElementById("closeNav").style.opacity ="1";
     document.getElementById("toggle").style.display ="none";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-    document.getElementById("myNav").style.top = "100%";
+    document.getElementById("myNav").style.left = "100%";
     document.getElementById("closeNav").style.opacity ="0";
     document.getElementById("toggle").style.display ="block";
 }
