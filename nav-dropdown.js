92580a26af8216fe9d26c4eac8456ffc9bf4de76
Unknown <aron27967460@hotmail.com>
2019-02-07 07:44:08 -0800

    font change, index update

--- nav-dropdown.js
+++ nav-dropdown.js
@@ -1,11 +1,13 @@
 function openNav() {
-    document.getElementById("myNav").style.left = "0%";
+    document.getElementById("myNav").style.opacity = "0.95";
+    document.getElementById("myNav").style.visibility = "visible";
     document.getElementById("closeNav").style.opacity ="1";
     document.getElementById("toggle").style.display ="none";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.left = "100%";
+    document.getElementById("myNav").style.opacity = "0";
+    document.getElementById("myNav").style.visibility = "hidden";
     document.getElementById("closeNav").style.opacity ="0";
     document.getElementById("toggle").style.display ="block";
 }
