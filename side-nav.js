b1cfdab2b058b617c0ab579791fc972c477a8f41
Unknown <aron27967460@hotmail.com>
2019-07-05 16:26:24 -0700

    mobile project-item style update

--- side-nav.js
+++ side-nav.js
@@ -19,11 +19,3 @@ function scrollFunction() {
         document.getElementById("sidebar-6").style.opacity = "0";
     }
 }
-
-
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
