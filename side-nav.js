0e3161a35d0fb802a5b0be4cc318acf0a028ee77
Unknown <aron27967460@hotmail.com>
2019-07-25 23:07:49 -0700

    plugged in back-to-top button into mobile

--- side-nav.js
+++ side-nav.js
@@ -1,21 +1,49 @@
 window.onscroll = function () {scrollFunction();};
 
+// function scrollFunction() {
+//     if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+//         document.getElementById("topBtn").style.opacity = "1";
+//         document.getElementById("sidebar-1").style.opacity = "1";
+//         document.getElementById("sidebar-2").style.opacity = "1";
+//         document.getElementById("sidebar-3").style.opacity = "1";
+//         document.getElementById("sidebar-4").style.opacity = "1";
+//         document.getElementById("sidebar-5").style.opacity = "1";
+//         document.getElementById("sidebar-6").style.opacity = "1";
+//     }else {
+//         document.getElementById("topBtn").style.opacity = "0";
+//         document.getElementById("sidebar-1").style.opacity = "0";
+//         document.getElementById("sidebar-2").style.opacity = "0";
+//         document.getElementById("sidebar-3").style.opacity = "0";
+//         document.getElementById("sidebar-4").style.opacity = "0";
+//         document.getElementById("sidebar-5").style.opacity = "0";
+//         document.getElementById("sidebar-6").style.opacity = "0";
+//     }
+// }
+
 function scrollFunction() {
     if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-        document.getElementById("topBtn").style.opacity = "1";
-        document.getElementById("sidebar-1").style.opacity = "1";
-        document.getElementById("sidebar-2").style.opacity = "1";
-        document.getElementById("sidebar-3").style.opacity = "1";
-        document.getElementById("sidebar-4").style.opacity = "1";
-        document.getElementById("sidebar-5").style.opacity = "1";
-        document.getElementById("sidebar-6").style.opacity = "1";
+        document.getElementById("topBtn").style.display = "block";
+        document.getElementById("sidebar-1").style.display = "block";
+        document.getElementById("sidebar-2").style.display = "block";
+        document.getElementById("sidebar-3").style.display = "block";
+        document.getElementById("sidebar-4").style.display = "block";
+        document.getElementById("sidebar-5").style.display = "block";
+        document.getElementById("sidebar-6").style.display = "block";
     }else {
-        document.getElementById("topBtn").style.opacity = "0";
-        document.getElementById("sidebar-1").style.opacity = "0";
-        document.getElementById("sidebar-2").style.opacity = "0";
-        document.getElementById("sidebar-3").style.opacity = "0";
-        document.getElementById("sidebar-4").style.opacity = "0";
-        document.getElementById("sidebar-5").style.opacity = "0";
-        document.getElementById("sidebar-6").style.opacity = "0";
+        document.getElementById("topBtn").style.display = "none";
+        document.getElementById("sidebar-1").style.display = "none";
+        document.getElementById("sidebar-2").style.display = "none";
+        document.getElementById("sidebar-3").style.display = "none";
+        document.getElementById("sidebar-4").style.display = "none";
+        document.getElementById("sidebar-5").style.display = "none";
+        document.getElementById("sidebar-6").style.display = "none";
     }
+}
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
