0489bc57c56ef0b2c4c11677483121013770b8d3
Unknown <aron27967460@hotmail.com>
2024-12-16 15:02:33 -0800

    tab + tab scrolling update

--- side-nav.js
+++ side-nav.js
@@ -1,24 +1,5 @@
 window.onscroll = function () {scrollFunction();};
 
-// function scrollFunction() {
-//     if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-//         document.getElementById("topBtn").style.opacity = "1";
-//         document.getElementById("sidebar-1").style.opacity = "1";
-//         document.getElementById("sidebar-2").style.opacity = "1";
-//         document.getElementById("sidebar-3").style.opacity = "1";
-//         document.getElementById("sidebar-4").style.opacity = "1";
-//         document.getElementById("sidebar-5").style.opacity = "1";
-//         document.getElementById("sidebar-6").style.opacity = "1";
-//     }else {
-//         document.getElementById("topBtn").style.opacity = "0";
-//         document.getElementById("sidebar-1").style.opacity = "0";
-//         document.getElementById("sidebar-2").style.opacity = "0";
-//         document.getElementById("sidebar-3").style.opacity = "0";
-//         document.getElementById("sidebar-4").style.opacity = "0";
-//         document.getElementById("sidebar-5").style.opacity = "0";
-//         document.getElementById("sidebar-6").style.opacity = "0";
-//     }
-// }
 
 function scrollFunction() {
     if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
